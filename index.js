2813fa3310a04fc18901e0114c84f71889b1a30c
Roy-Weiss <67227056+Roy-Weiss@users.noreply.github.com>
2020-08-07 21:02:27 -0400

    Task 1,2

--- index.js
+++ index.js
@@ -29,25 +29,32 @@ console.log(fifaData[828]["Win conditions"]);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals( /* code here */ ) {
-
-    /* code here */
-
+function getFinals(data) {
+    const finalD = data.filter((element) => {
+        return element.stage === "Final"
+    });
+    return finalD;
 };
+
+
+
+console.log(getFinals(fifaData));
+
+
+
+
+
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears( /* code here */ ) {
 
-    /* code here */
 
-};
 
-getYears();
+
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */
 
-function getWinners( /* code here */ ) {
+function getWinners(fifaData, ) {
 
     /* code here */
 
@@ -62,21 +69,13 @@ Parameters:
  * callback function getYears
  */
 
-function getWinnersByYear( /* code here */ ) {
-
-};
-
-getWinnersByYear();
+function getWinnersByYear(data, callback) {}
 
 /* Task 6: Write a function called `getAverageGoals` that accepts a parameter `data` and returns the the average number of home team goals and away team goals scored per match (Hint: use .reduce and do this in 2 steps) */
 
-function getAverageGoals( /* code here */ ) {
 
-    /* code here */
 
-};
-
-getAverageGoals();
+getAverageGoals(fifaData);
 
 /// STRETCH 🥅 //
 
